2cfb28c2d183fcc5d3652cc9ce59ab51bd8e690b
Dave Shah <david.a.shah@gmail.com>
2018-02-26 22:34:00 -0500

    Trying a stringy approach

--- lib/stubby.ex
+++ lib/stubby.ex
@@ -4,15 +4,40 @@ defmodule Stubby do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Stubby.hello
-      :world
-
   """
-  def hello do
-    :world
+  # TODO: take erlang version into consideration
+  def collect_callbacks(module) do
+    module.module_info[:attributes]
+    |> Keyword.take([:behaviour])
+    |> Keyword.values
+    |> List.flatten
+    |> Enum.reduce([], fn(b, acc) ->
+      acc ++ b.behaviour_info(:callbacks) end)
+  end
+
+  def function_signatature(0), do: "()"
+  def function_signatature(arity) do
+    sig =
+      Range.new(1,arity)
+      |> Enum.reduce("", fn(a,acc) -> acc <> "arg#{a}," end)
+      |> String.trim_trailing(",")
+
+    "(#{sig})"
+  end
+
+  defmacro __using__(_args) do
+    #TODO: generate (arg1, arg2,... ) based on arity
+    # generate ets calls based on arity
+    """
+    def foo(a,b) do
+      IO.puts a
+      IO.puts b
+    end
+
+    def bar() do
+      IO.puts "bar"
+    end
+    """
+    |> Code.string_to_quoted!
   end
 end
--- test/stubby_test.exs
+++ test/stubby_test.exs
@@ -2,7 +2,46 @@ defmodule StubbyTest do
   use ExUnit.Case
   doctest Stubby
 
-  test "greets the world" do
-    assert Stubby.hello() == :world
+  defmodule FakeBehaviour2 do
+    @callback fake_function2(term) :: {:ok, term} | {:error, String.t}
+    @callback fake_function2(term, term) :: {:ok, term} | {:error, String.t}
+  end
+
+  defmodule FakeBehaviour do
+    @callback fake_function(term) :: {:ok, term} | {:error, String.t}
+    @callback fake_function(term, term) :: {:ok, term} | {:error, String.t}
+  end
+
+  defmodule FakeStub do
+    @behaviour FakeBehaviour
+    @behaviour FakeBehaviour2
+    use Stubby
+  end
+
+  describe "collect_callbacks/1" do
+    test "returning all the callbacks within a module" do
+      assert Stubby.collect_callbacks(FakeStub) ==
+             [fake_function: 2, fake_function: 1,
+              fake_function2: 2, fake_function2: 1]
+    end
+  end
+  describe "function_signature/1" do
+    test "returns a signature string with a matching arity" do
+      assert Stubby.function_signatature(0) == "()"
+      assert Stubby.function_signatature(1) == "(arg1)"
+      assert Stubby.function_signatature(3) == "(arg1,arg2,arg3)"
+    end
+  end
+
+
+  # TODO: In Elixir version 1.3 callbacks can be found in module_info
+  # in Elixir ??? they're in behaviour_info
+  # NOTE: these are NOT public functions. These are
+  # exports found from
+  # Stubby.FakeBehaviour.behaviour_info(:callbacks)
+  # :application.which_applications can give me the info I need...
+  test "function generation" do
+    #require IEx; IEx.pry
+    FakeStub
   end
 end
