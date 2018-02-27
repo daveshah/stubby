3094cce79c018eff9ba56383f07bb5634203c9d2
Dave Shah <david.a.shah@gmail.com>
2018-02-27 17:56:30 -0500

    Update package

--- mix.exs
+++ mix.exs
@@ -1,28 +1,29 @@
 defmodule Stubby.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :stubby,
-      version: "0.1.0",
+      version: @version,
       elixir: ">= 1.3.0",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+  defp package do
+    %{
+      licenses: ["Apache 2"],
+      maintainers: ["Dave Shah"],
+      links: %{"GitHub" => "https://github.com/daveshah/stubby"}
+    }
   end
 end
